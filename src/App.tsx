6ca0b1dfb65ab40f7be7bbe79ade8824a17ee573
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-14 10:31:08 -0400

    Add router into app. Sets base routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Listings, CreateListing, UpdateListing, RemoveListing } from "./pages";
 
 function App() {
   return (
     <>
-      <Listings /> <CreateListing />
-      <UpdateListing />
-      <RemoveListing />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Listings />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:id" element={<UpdateListing />} />
+          <Route path="/remove-listing/:id" element={<RemoveListing />} />
+        </Routes>
+      </Router>
     </>
   );
 }
